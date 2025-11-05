17b5335cdc877eb036587df1e5496fa17408458e
Felix Yuan <felixyuan2003@outlook.com>
2025-11-05 22:09:54 +0800

    Update download_checkpoints.sh

--- download_checkpoints.sh
+++ download_checkpoints.sh
@@ -2,7 +2,7 @@ mkdir -p ./checkpoints/
 cd ./checkpoints/
 
 # megasam
-wget https://github.com/mega-sam/mega-sam/blob/main/checkpoints/megasam_final.pth -O checkpoints/megasam_final.pth
+wget https://github.com/mega-sam/mega-sam/blob/main/checkpoints/megasam_final.pth -O megasam_final.pth
 
 # depth anything
 huggingface-cli download --resume-download depth-anything/Depth-Anything-V2-Large --local-dir Depth-Anything
